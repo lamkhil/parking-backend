048125464bf2ae24cb1057227299080333ba89fc
lamkhil <lamkhil3000@gmail.com>
2025-09-09 09:20:23 +0700

    update api

--- app/Models/VehicleType.php
+++ app/Models/VehicleType.php
@@ -3,11 +3,26 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
 
 class VehicleType extends Model
 {
+
+    protected $appends = [
+        'image_url'
+    ];
+    
     public function parkingRateRules()
     {
         return $this->hasMany(ParkingRateRule::class);
+    }
+
+    public function getImageUrlAttribute()
+    {
+        if ($this->image == null) {
+            return null;
+        }
+
+        return Storage::url($this->image);
     }
 }
